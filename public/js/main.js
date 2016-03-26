820dd22b3122c13f9ce9aaa85a7da3d90cd55a20
luis <ferrerluis97@gmail.com>
2016-03-26 17:33:56 -0400

    Bunch of changes to CSS and JS. In JS, set up the modal when you click on a card

--- public/js/main.js
+++ public/js/main.js
@@ -5,9 +5,7 @@ function urlify(text) {
     if (text.match(imgRegex)) {
         
         return text.replace(imgRegex, function(url) {
-            return '<a href="' + url + '">' +
-                        '<img src="' + url + '">' +
-                '</a>';
+            return '<img src="' + url + '">';
         });
     }
     
@@ -16,17 +14,55 @@ function urlify(text) {
     });
 }
 
+function setClickListener(selector) {
+    
+    selector.click(function(html) {
+        var content = html.currentTarget.innerHTML;
+        console.log(content);
+        swal({
+            html: content,
+            customClass: 'expandable-modal',
+            width: window.innerWidth * 0.85
+        });
+    });
+}
+
 $(function(){ //DOM Ready
 
+    var socket = io();
+
+    socket.on('new msg', function(msg) {
+        
+        var item = $('<li>').addClass('message-card').html(urlify(msg));
+        
+        gridster.add_widget(item, 1, 1, 1, 1);
+        
+        setClickListener(item);
+    });
+    
+    var toolbarWidth = $('#toolbar').width();
+    
+    var widgetWidth = toolbarWidth/4 - 10;
+    var maxCol = 4;
+    
+    if (toolbarWidth <= 460) {
+        
+        widgetWidth = toolbarWidth;
+        maxCol = 1;
+    }
+    
+    
+    console.log(widgetWidth);
+
+    widgetWidth -= parseInt($('.message-card').css('padding'))*2;
+
     var gridster = $(".gridster ul").gridster({
-        widget_margins: [10, 10],
-        widget_base_dimensions: [140, 140]
+        widget_margins: [15, 15],
+        widget_base_dimensions: [widgetWidth, 80]
     }).data('gridster');
     
     gridster.disable();
-
-    var socket = io();
-        
+    
     $('#msg-field').keypress(function(event){
         if(event.keyCode == 13) {
             $("form").submit();
@@ -34,15 +70,13 @@ $(function(){ //DOM Ready
     });
 
     $('form').submit(function() {
-        socket.emit('new msg', $('#msg-field').val());
-        $('#msg-field').val('');
+        if (!$("#msg-field").val().match(/^\s*$/)) {
+            socket.emit('new msg', $('#msg-field').val());
+            $('#msg-field').val('');
+        }
+        
         return false;
     });
-
-    socket.on('new msg', function(msg) {
-        
-        var display = urlify(msg);
-        
-        gridster.add_widget('<li class="message-card">' + display + '</li>', 2, 1, 1, 1);
-});
+    
+    setClickListener($('.message-card'));
 });
