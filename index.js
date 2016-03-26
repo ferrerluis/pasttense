e035475453182b018dd7c6a6c47617f0a5608288
luis <ferrerluis97@gmail.com>
2016-03-26 05:10:57 -0400

    Playing with socket.io. Able to send messages from client to server. Created 'public' folder to put css and js static files

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
-var app = require("express")();
+var express = require("express");
+var app = express();
 var http = require("http").Server(app);
 var io = require('socket.io')(http);
+
+app.use(express.static('public'));
 
 app.get("/", function(req, res){
 	res.sendFile(__dirname + "/index.html");
 });
 
 io.on('connection', function(socket){
-  console.log('a user connected');
+	console.log("user connected");    
+    socket.on('new msg', function(msg){
+        console.log('message: ' + msg);
+    });
 });
 
-
-http.listen(5000, function(){
+http.listen(3000, function(){
 	console.log("listening on 3000");
 });
